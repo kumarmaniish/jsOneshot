e54922b1da5fd0ed512beb7eee73462b1fdff8e4
Manish Kumar <133526263+kumarmaniish@users.noreply.github.com>
2024-05-04 17:53:14 +0000

    Conversion of datatypes

---
+++ 01_basics/03_dataConversion.js
@@ -0,0 +1,40 @@
+let score2 = "33";
+
+// if score = 33abc then shall it convert it to number? No but it happens
+// let me check
+
+let score1="33abc"
+
+// for null
+let score3=null;
+let score4=true; // 1 or 0
+let score=undefined
+
+/*console.log(typeof score);
+console.log(typeof(score));
+
+let value=Number(score); // typecasting
+console.log(typeof value);// number
+console.log(value); // NaN
+
+
+/* "33" ==> 33
+"33abc" ==> NaN
+true =>1, false=>0;*/
+
+let isLoggedIn="manish"
+let booleanIsloggedIn=Boolean(isLoggedIn);
+
+console.log(booleanIsloggedIn);
+
+// 1 -> true 0 -> false
+// "" ->false
+// "manish"->true
+
+
+let someN=33;
+let stringSomeN=String(someN);
+
+console.log(typeof stringSomeN);
+console.log(stringSomeN);
+
