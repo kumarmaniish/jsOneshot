5fb4566fda1df24686be58b7ec9c7cd05390cb75
Manish Kumar <133526263+kumarmaniish@users.noreply.github.com>
2024-05-05 10:56:02 +0000

    summaryDatatypes

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>3);
+
+// console.log("2" > 3)
+
+
+// should be avoided. must write clean code
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// strict check
+
+console.log(2 == 2);
+console.log("2" === 2);
---
+++ 01_basics/datatypeS.js
@@ -0,0 +1,69 @@
+// primitive--> call by value (whatever we chanegs made then changes made on copy of the data
+// only not the memory refernce)
+
+// 7 type : Number, string, boolean , null , undefined, symbol(kisi bhi value ko unique bannae 
+//ke liye use hota hai)
+// BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outSideTemp = null;
+
+let userEmail;// undefined
+let userId=undefined;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+// console.log(id === anotherId);
+
+const bigNumber = 3456765437823456789876n;
+// javascript is a dynamically type language where the interpreter assigns variables a type at runtime
+// based on the variable's value at the time.
+
+
+//  non primtive or refernce type
+
+// Arrays, objects, Functions
+
+// arrays
+const heros = ["shaktiman","naagaraj","Doga"];
+
+
+// objects
+let myObj = {
+    name:"Manish",
+    age: 22,
+}
+
+// functions
+const myFunctions=function(){
+    console.log("hello World");
+}
+
+// console.log(myFunctions)
+
+// to identify the data type
+console.log(typeof bigNumber)
+console.log(typeof outSideTemp)
+console.log(typeof myFunctions)
+
+
+/* Summary of datatypes
+
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object */
