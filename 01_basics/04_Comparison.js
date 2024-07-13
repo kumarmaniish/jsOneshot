7e979827ffc493ba57f324f863aa21cfe896c877
Manish Kumar <133526263+kumarmaniish@users.noreply.github.com>
2024-07-13 13:44:57 +0530

    Update 04_Comparison.js

--- 01_basics/04_Comparison.js
+++ 01_basics/04_Comparison.js
@@ -1,19 +1,20 @@
 // console.log(2>3);
 
-// console.log("2" > 3)
+// console.log("2" > 3) // convert into number then return false;
 
 
 // should be avoided. must write clean code
-// console.log(null == 0);
-// console.log(null >= 0);
+// console.log(null == 0); //false
+// console.log(null >= 0); // true
+console.log(53==55==1); // false -> 53==55 ->0, 0==1->false;
+console.log(55==55==1); //true;
 
 
-
-// console.log(undefined == 0);
-// console.log(undefined < 0);
-// console.log(undefined > 0);
+// console.log(undefined == 0); //false
+// console.log(undefined < 0); //false
+// console.log(undefined > 0); //false
 
 // strict check
 
-console.log(2 == 2);
-console.log("2" === 2);
+console.log(2 == 2); //true
+console.log("2" === 2); //false
